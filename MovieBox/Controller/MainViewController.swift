a3eae836d876be294425ae140ee4341453ddd080
Yerkebulan <elzhanerke03@gmail.com>
2020-12-23 22:09:09 +0600

    first init

---
+++ MovieBox/Controller/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieBox
+//
+//  Created by erkebulan elzhan on 12/23/20.
+//  Copyright © 2020 Yerkebulan Yelzhan. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
